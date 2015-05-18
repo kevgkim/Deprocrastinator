c3c0bfd9688cb75b9c2c70dda2f923f9448e6989
Kevin Kim <kevimkin@gmail.com>
2015-05-18 14:51:36 -0700

    The table is built and four cells are filld with an array

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,20 +8,29 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+@property NSArray *toDoList;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.toDoList = [[NSArray alloc] initWithObjects:@"Walk the dog", @"Do the dishes", @"Kick the cat", @"Give Max a high five", nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSLog(@"hello");
+    return self.toDoList.count;
 }
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    cell.textLabel.text = [self.toDoList objectAtIndex:indexPath.row];
+    return cell;
+}
+
 
 @end
